4a5fdb8a4ddac5c5a6d2482c961d967222693ea8
Xabier Abaunz Gallastegui <157809838+XabiAbaunz@users.noreply.github.com>
2024-09-29 13:03:49 +0000

    Aldaketak

--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
-document.onload = () => {
-    const l = "https://covers.openlibrary.org/b/id/"
-    const db = require('./datubasea.json');
-    console.log(db);
-    let indizea = 0;
-    let izenburua = document.querySelector('p:nth-child(1) > input[type=text]');
-    let egilea = document.querySelector('p:nth-child(2) > input[type=text]');
-    let data = document.querySelector('p:nth-child(3) > input[type=text]');
-    let isbn = document.querySelector('p:nth-child(4) > input[type=text]');
 
-    let irudia = document.getElementById('img');
+window.onload = () => {
 
-    izenburua.value = db[indizea].izenburua;
-    egilea.value = db[indizea].egilea;
-    data.value = db[indizea].data;
-    isbn.value = db[indizea].isbn;
-    irudia.src = l + db[indizea].irudia;
+    fetch('../datubasea.json')
+    .then(response => response.json())
+    .then(data => {
+        const db = data;
 
+        const l = "https://covers.openlibrary.org/b/id/"
+        let indizea = 0;
+        let izenburua = document.querySelector('p:nth-child(1) > input[type=text]');
+        let egilea = document.querySelector('p:nth-child(2) > input[type=text]');
+        let d = document.querySelector('p:nth-child(3) > input[type=text]');
+        let isbn = document.querySelector('p:nth-child(4) > input[type=text]');
+
+        let eskuin = document.getElementById('eskuin');
+        let ezker = document.getElementById('ezker');
+
+        let irudia = document.getElementById('img');
+
+        balioakAldatu = (indizea) => {
+            console.log(indizea)
+            izenburua.value = db[indizea].izenburua
+            egilea.value = db[indizea].egilea
+            d.value = db[indizea].data
+            isbn.value = db[indizea].isbn
+            irudia.src = l + db[indizea].filename
+        }
+
+        clickEzker = () => {
+            console.log("Ezker botoia sakatu da")
+            if(indizea == 0) {
+                indizea = db.length - 1;
+            } else {
+                indizea--;
+            }
+            balioakAldatu(indizea);
+        }
+
+        clickEskuin = () => {
+            console.log("Eskuin botoia sakatu da")
+            if(indizea == db.length - 1) {
+                indizea = 0;
+            } else {
+                indizea++;
+            }
+            balioakAldatu(indizea);
+        }
+
+        ezker.addEventListener('click', clickEzker);
+        eskuin.addEventListener('click', clickEskuin);
+        
+    })
+    .catch(error => console.error('Error al cargar el JSON:', error));
     
 }
