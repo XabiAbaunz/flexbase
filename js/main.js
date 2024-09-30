aae369a0aa44954b62cee3349655068ce9078cb3
Xabier Abaunz Gallastegui <157809838+XabiAbaunz@users.noreply.github.com>
2024-09-30 10:44:56 +0000

    .

--- js/main.js
+++ js/main.js
@@ -1,51 +1,55 @@
-
 window.onload = () => {
+    
+    const l = "https://covers.openlibrary.org/b/id/"
+    let indizea = 0;
+    let izenburua = document.querySelector('p:nth-child(1) > input[type=text]');
+    let egilea = document.querySelector('p:nth-child(2) > input[type=text]');
+    let d = document.querySelector('p:nth-child(3) > input[type=text]');
+    let isbn = document.querySelector('p:nth-child(4) > input[type=text]');
+
+    let eskuin = document.getElementById('eskuin');
+    let ezker = document.getElementById('ezker');
+
+    let irudia = document.getElementById('img');
+
+
+    const clickEzker = () => {
+        console.log("Ezker botoia sakatu da")
+        if(indizea == 0) {
+            indizea = db.length - 1;
+        } else {
+            indizea--;
+        }
+        balioakAldatu(indizea);
+    };
+
+    const clickEskuin = () => {
+        console.log("Eskuin botoia sakatu da")
+        if(indizea == db.length - 1) {
+            indizea = 0;
+        } else {
+            indizea++;
+        }
+        balioakAldatu(indizea);
+    };
+
+    const balioakAldatu = (indizea) => {
+        console.log(indizea)
+        izenburua.value = db[indizea].izenburua
+        egilea.value = db[indizea].egilea
+        d.value = db[indizea].data
+        isbn.value = db[indizea].isbn
+        irudia.src = l + db[indizea].filename
+    };
+
+    let db;
 
     fetch('../datubasea.json')
     .then(response => response.json())
     .then(data => {
-        const db = data;
+        db = data;
 
-        const l = "https://covers.openlibrary.org/b/id/"
-        let indizea = 0;
-        let izenburua = document.querySelector('p:nth-child(1) > input[type=text]');
-        let egilea = document.querySelector('p:nth-child(2) > input[type=text]');
-        let d = document.querySelector('p:nth-child(3) > input[type=text]');
-        let isbn = document.querySelector('p:nth-child(4) > input[type=text]');
-
-        let eskuin = document.getElementById('eskuin');
-        let ezker = document.getElementById('ezker');
-
-        let irudia = document.getElementById('img');
-
-        balioakAldatu = (indizea) => {
-            console.log(indizea)
-            izenburua.value = db[indizea].izenburua
-            egilea.value = db[indizea].egilea
-            d.value = db[indizea].data
-            isbn.value = db[indizea].isbn
-            irudia.src = l + db[indizea].filename
-        }
-
-        clickEzker = () => {
-            console.log("Ezker botoia sakatu da")
-            if(indizea == 0) {
-                indizea = db.length - 1;
-            } else {
-                indizea--;
-            }
-            balioakAldatu(indizea);
-        }
-
-        clickEskuin = () => {
-            console.log("Eskuin botoia sakatu da")
-            if(indizea == db.length - 1) {
-                indizea = 0;
-            } else {
-                indizea++;
-            }
-            balioakAldatu(indizea);
-        }
+        balioakAldatu(0);
 
         ezker.addEventListener('click', clickEzker);
         eskuin.addEventListener('click', clickEskuin);
